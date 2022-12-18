266595ed42c10bfc93562d8c0f2522cf76b306ee
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-12-18 15:39:33 +0100

    ini

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h2>Poster un message</h2>
+			<form>
+				<input
+					type="text"
+					placeholder="Entrez un message"
+					id="inputMessage"
+				/>
+				<input type="submit" value="Envoyer" />
+			</form>
+			<h2>Liste des messages</h2>
+			<div></div>
+		</div>
+	);
+};
 
 export default App;
